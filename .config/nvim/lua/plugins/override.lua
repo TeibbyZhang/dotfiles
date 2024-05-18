a9f2ffcb70c7b982aa48397b7171d6f8e0c286c8
TeibbyZhang <teibbyzhang@gmail.com>
2024-05-19 01:38:42 +0800

    feat(nvim): add some config

--- .config/nvim/lua/plugins/override.lua
+++ .config/nvim/lua/plugins/override.lua
@@ -18,4 +18,16 @@ return {
       use_diagnostic_signs = true,
     },
   },
+  {
+    'lukas-reineke/indent-blankline.nvim',
+    opts = function(_, opts)
+      -- Other blankline configuration here
+      return require('indent-rainbowline').make_opts(opts, {
+        color_transparency = 0.18,
+      })
+    end,
+    dependencies = {
+      'TheGLander/indent-rainbowline.nvim',
+    },
+  },
 }
